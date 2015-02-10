8c5dcc160423c22913a46efb942d85a5ec698bde
Sanjeev Koranga <skoranga@paypal.com>
2015-02-10 14:04:04 -0800

    using the right async parseString api in test

--- benchmark/README.md
+++ benchmark/README.md
@@ -10,17 +10,16 @@
 
 #### On Node 0.10.36
 
- - xml2js_0.4.4 - simple x 32,653 ops/sec ±0.99% (89 runs sampled)
- - xml2js_perf - simple x 33,222 ops/sec ±1.13% (91 runs sampled)
- - xml2js_0.4.4 - complex x 5,158 ops/sec ±1.24% (90 runs sampled)
- - xml2js_perf - complex x 5,343 ops/sec ±0.96% (91 runs sampled)
+ - xml2js_0.4.4 - simple x 536 ops/sec ±1.81% (77 runs sampled)
+ - xml2js_perf - simple x 548 ops/sec ±2.25% (72 runs sampled)
+ - xml2js_0.4.4 - complex x 411 ops/sec ±1.74% (51 runs sampled)
+ - xml2js_perf - complex x 409 ops/sec ±1.78% (39 runs sampled)
 
 #### On Node 0.12.0
 
- - xml2js_0.4.4 - simple x 18,545 ops/sec ±1.86% (85 runs sampled)
- - xml2js_perf - simple x 19,299 ops/sec ±1.42% (85 runs sampled)
- - xml2js_0.4.4 - complex x 3,812 ops/sec ±1.69% (87 runs sampled)
- - xml2js_perf - complex x 3,774 ops/sec ±1.36% (92 runs sampled)
-
+ - xml2js_0.4.4 - simple x 476 ops/sec ±2.31% (74 runs sampled)
+ - xml2js_perf - simple x 483 ops/sec ±2.73% (68 runs sampled)
+ - xml2js_0.4.4 - complex x 391 ops/sec ±1.53% (39 runs sampled)
+ - xml2js_perf - complex x 389 ops/sec ±3.61% (40 runs sampled)
 
 Overall performing bad as compared to Node@0.10.36 and not seeing much difference in Node@0.12.0
--- benchmark/perf.js
+++ benchmark/perf.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
 
-    var Benchmark = require('benchmark');
-    var suite = new Benchmark.Suite();
+    var async = require('async');
+    var benchmark = require('async-benchmark');
 
     var parseString_044 = require('xml2js').parseString;
     var parseString_perf = require('../lib/xml2js').parseString;
@@ -32,6 +32,55 @@
           "</arraytest>" +
           "<emptytest>  </emptytest></root>";
 
+  var benchParseString_044_Simple = function (done) {
+      parseString_044(simpleXML, function(err, result) {
+        done();
+      });
+    },
+    benchParseString_perf_Simple = function (done) {
+      parseString_perf(simpleXML, function(err, result) {
+        done();
+      });
+    },
+    benchParseString_044_Complex = function (done) {
+      parseString_044(complexXML, function(err, result) {
+        done();
+      });
+    },
+    benchParseString_perf_Complex = function (done) {
+      parseString_perf(complexXML, function(err, result) {
+        done();
+      });
+    };
+
+    async.series([
+        function (cb) {
+          benchmark('xml2js_0.4.4 - simple', benchParseString_044_Simple, function (err, event) {
+            console.log(event.target.toString());
+            cb();
+          });
+        },
+        function(cb) {
+          benchmark('xml2js_perf - simple', benchParseString_perf_Simple, function (err, event) {
+            console.log(event.target.toString());
+            cb();
+          });
+        },
+        function (cb) {
+          benchmark('xml2js_0.4.4 - complex', benchParseString_044_Complex, function (err, event) {
+            console.log(event.target.toString());
+            cb();
+          });
+        },
+        function(cb) {
+          benchmark('xml2js_perf - complex', benchParseString_perf_Complex, function (err, event) {
+            console.log(event.target.toString());
+            cb();
+          });
+        }
+      ]);
+
+/*
     suite.add('xml2js_0.4.4 - simple', function() {
         parseString_044(simpleXML);
     })
@@ -48,4 +97,5 @@
         console.log(String(event.target));
     })
     .run({ 'async': true });
+    */
 })();
